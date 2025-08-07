3da010509bc8f2be2214f7695fd206ffe314542f
APICE09 <gameiceza5524@gmail.com>
2025-08-07 11:41:47 +0700

    11.41

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -27,10 +27,10 @@
         <nav id="navmenu" class="navmenu">
           <ul>
             <li><a href="#hero" class="active">Home</a></li>
-            <li><a href="#about">About</a></li>
+            <li><a href="{{ route('about.reserchers') }}">About</a></li>
             <li><a href="#services">Services</a></li>
             <li><a href="#portfolio">Portfolio</a></li>
-            <li><a href="#team">Team</a></li> --}}
+            {{-- <li><a href="#team">Team</a></li> --}}
             <li class="dropdown"><a href="#"><span>เกี่ยวกับโครงการ</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
               <ul>
                 <li><a href="#">เกี่ยวกับโครงการ</a></li>
