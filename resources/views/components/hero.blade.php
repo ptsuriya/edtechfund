f8d2c00c0feab5052b157dfb84bcbfaf8df64cf0
APICE09 <gameiceza5524@gmail.com>
2025-08-25 11:17:09 +0700

    banner

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,19 +1,36 @@
         <!-- Hero Section -->
-    <section id="hero" class="hero section light-background">
+        <section id="hero" class="hero section light-background">
 
-      <div class="container">
-        <div class="row gy-4 quoat">
-            <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center" data-aos="zoom-out">
+            <div class="container">
+                <div class="row gy-4 quoat">
+                    <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center"
+                        data-aos="zoom-out">
 
+                    </div>
+                    <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center"
+                        data-aos="zoom-out">
+                        <div class="card shadow-lg border-0 bg-white bg-opacity-75">
+                            <div class="card-body text-center">
+                                <h2 class="card-title">
+                                    <i>
+                                        " <span class="me-2">ข้าพเจ้า </span>
+                                        พร้อมที่จะสนับสนุนทุกทางที่ทำได้ เพื่อเราจะได้เป็นกำลังสำคัญอีกส่วนหนึ่ง
+                                        ในการสร้างชาติบ้านเมืองของเรา ให้มั่นคง และรุ่งเรื่องยิ่งขึ้น "
+                                    </i>
+                                </h2>
+                                <p class="card-text mt-3">
+                                    พระราชดำรัสสมเด็จพระนางเจ้ารำไพพรรณีฯ
+                                    ในพิธีพระราชทานประกาศนียบัตรของวิทยาลัยครูจันทบุรี รุ่นที่ 2
+                                    ณ วันที่ 30 มีนาคม 2515
+                                </p>
+                                <div class="d-flex justify-content-center mt-3">
+                                    <a href="#pricing" class="btn btn-primary">เข้าสู่ระบบ</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </div>
-          <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center" data-aos="zoom-out">
-            <h2><i> " <span class="me-2">ข้าพเจ้า </span>พร้อมที่จะสนับสนุนทุกทางที่ทำได้ เพื่อเราจะได้เป็นกำลังสำคัญอีกส่วนหนึ่ง ในการสร้างชาติบ้านเมืองของเรา ให้มั่นคง และรุ่งเรื่องยิ่งขึ้น " </i></h2>
-            <p>พระราชดำรัสสมเด็จพระนางเจ้ารำไพพรรณีฯ ในพิธีพระราชทานประกาศนียบัตรของวิทยาลัยครูจันทบุรี รุ่นที่ 2 ณ วันที่ 30 มีนาคม 2515 </p>
-            <div class="d-flex justify-content-center">
-              <a href="#pricing" class="btn-get-started">เข้าสู่ระบบ</a>
-            </div>
-          </div>
-        </div>
-      </div>
 
-    </section><!-- /Hero Section -->
+        </section><!-- /Hero Section -->
